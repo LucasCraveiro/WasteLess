5645bfef8ebd26347626305e373385a097782b4b
Lucas Craveiro <lucashcraveiro@gmail.com>
2018-06-21 13:44:44 -0700

    Added Struct Food

---
+++ WasteLess/Food.swift
@@ -0,0 +1,16 @@
+//
+//  Food.swift
+//  WasteLess
+//
+//  Created by Lucas Craveiro on 2018-06-21.
+//  Copyright © 2018 Lucas Craveiro. All rights reserved.
+//
+
+import Foundation
+
+struct Food {
+    var id : Int
+    var name : String
+    var expiryDate: Date
+    var isExpired: Bool
+}
